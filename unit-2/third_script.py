30ca69b4432cdc6a229c0ba95f142613badc9f21
Wladimir Eduardo Banda Barragán <wbandabarragan@gmail.com>
2026-02-10 18:29:20 -0500

    Add files via upload

---
+++ unit-2/third_script.py
@@ -0,0 +1,22 @@
+# This is our second script
+
+# Function
+def print_message(msg):
+    """
+    Print the string (msg) provided by the user
+    Input: String (msg)
+    Output: print statement
+    """
+    print("Hola, " + msg)
+
+# Dictionary
+physics_courses = {
+  "level": "BSc",
+  "course": "electrodynamics I",
+  "semester": 5,
+  "programme": "physics/nanotechnology"
+}
+
+if __name__ == "__main__":
+   
+   print_message("Hola")
